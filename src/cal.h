4630421fad74066cf13235a6ad0bfa2803cf6a53
Ivaylo Dimitrov <freemangordon@abv.bg>
2013-01-29 21:46:24 +0000

    Add licensing

--- src/cal.h
+++ src/cal.h
@@ -1,3 +1,26 @@
+/**
+   @file cal.h
+
+   @brief Maemo CALibration data access library
+
+   Copyright (C) 2012 Ivaylo Dimitrov <freemangordon@abv.bg>
+
+   This file is part of libcal.
+
+   this library is free software;
+   you can redistribute it and/or modify it under the terms of the
+   GNU Lesser General Public License version 2.1 as published by the
+   Free Software Foundation.
+
+   libcal is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with osso-systemui-powerkeymenu.
+   If not, see <http://www.gnu.org/licenses/>.
+*/
 #ifndef __CAL_H__
 #define __CAL_H__
 
