d4c5fd9293ddb693c9b032b4c084cd0343b3ea26
Ivaylo Dimitrov <freemangordon@abv.bg>
2013-11-09 21:59:33 +0000

    Yet another typo
    Signed-off-by: Ivaylo Dimitrov <freemangordon@abv.bg>

--- cal.h
+++ cal.h
@@ -18,7 +18,7 @@
    Lesser General Public License for more details.
 
    You should have received a copy of the GNU Lesser General Public
-   License along with osso-systemui-powerkeymenu.
+   License along with libcal.
    If not, see <http://www.gnu.org/licenses/>.
 */
 
